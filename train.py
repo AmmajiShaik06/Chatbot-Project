dd83bcbe2b97dd17d07348f64ed5990de9c8e120
AmmajiShaik06 <ammajishaik0611@gmail.com>
2026-06-12 19:55:11 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,39 @@
+import json
+import pickle
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+patterns = []
+tags = []
+
+for intent in data["intents"]:
+
+    for pattern in intent["patterns"]:
+
+        patterns.append(pattern)
+
+        tags.append(intent["tag"])
+
+vectorizer = TfidfVectorizer()
+
+X = vectorizer.fit_transform(patterns)
+
+model = LogisticRegression()
+
+model.fit(X, tags)
+
+pickle.dump(
+    model,
+    open("model.pkl", "wb")
+)
+
+pickle.dump(
+    vectorizer,
+    open("vectorizer.pkl", "wb")
+)
+
+print("Training Completed")
